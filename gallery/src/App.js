c93ed5ff548a36a99cf5e710062a86a25c6d3e47
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-09 22:00:02 -0400

    Agregando estilo a los square

--- gallery/src/App.js
+++ gallery/src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import React from 'react'
 import Photo from './photo'
+import { useEffect } from 'react';
 
 function App() {
+
   return (
     <div className="App">
       <main className='bento-square'>
         <h1>Gallery</h1>
         <p>Using BentoUI</p>
-        <Photo />
       </main>
+      <Photo />
     </div>
   );
 }
--- gallery/src/photo.jsx
+++ gallery/src/photo.jsx
@@ -3,16 +3,16 @@ import { useEffect, useState } from 'react';
 
 export default function Photo() {
 
-    const [photos, setPhotos] = useState('https://picsum.photos/200/300')
+    const [photos, setPhotos] = useState('https://picsum.photos/300/300')
 
     //useEffect renderiza el componente solo una vez despues de que se monta el componente
     useEffect(() => {
-
+        return
     })
 
     return (
-        <div>
-            <img src={photos} alt=""/>
+        <div className='bento-square'>
+            <img src={photos} alt="Foto Random"/>
         </div>
     );
 }
