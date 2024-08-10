0e4aaaacbe0dac7057fb5c6cb204469bac0ca43d
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-09 22:45:44 -0400

    Listo

--- gallery/src/App.js
+++ gallery/src/App.js
@@ -5,12 +5,40 @@ import { useEffect } from 'react';
 
 function App() {
 
+  useEffect(() => {
+    document.querySelectorAll('img').forEach(img => {
+      img.src = img.src + `?random=${Math.random()}`;
+    });
+    document.querySelectorAll('.bento-square').forEach(square => {
+      square.style.width = (Math.random() + .2) * 20 + 'em'
+      square.onmouseover = () => {
+        square.style.width = parseInt(square.style.width.replace('em','')) + 5 + 'em'
+      }
+      square.onmouseout = () => {
+        square.style.width = parseInt(square.style.width.replace('em','')) - 5 + 'em'
+      }
+    })
+    document.querySelectorAll('.bento-square')[0].style.width = '20em'
+  })
+
   return (
     <div className="App">
       <main className='bento-square'>
         <h1>Gallery</h1>
         <p>Using BentoUI</p>
       </main>
+      <Photo />
+      <Photo />
+      <Photo />
+      <Photo />
+      <Photo />
+      <Photo />
+      <Photo />
+      <Photo />
+      <Photo />
+      <Photo />
+      <Photo />
+      <Photo />
       <Photo />
     </div>
   );
