240b6b8446d505352aeb5ab8086bdf4bdec68e3b
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-12 14:08:35 -0400

    Agregando Funcionalidad

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,12 +1,43 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Btn from './components/btn';
 
 function App() {
+
+
+  const handleClick = () => {
+    const btns = document.querySelectorAll('.btns button');
+
+    btns.forEach(btn => {
+      btn.addEventListener('click', () => {
+        const input = document.querySelector('input');
+        if(btn.textContent === 'C') {
+          input.value = '';
+          return
+        }
+        if(input.value === 'NaN' || input.value === 'Infinity') {
+          input.value = '';
+        }
+        if(input.value[input.value.length - 1] === '+' || input.value[input.value.length - 1] === '-' || input.value[input.value.length - 1] === '*' || input.value[input.value.length - 1] === '/' || input.value === '') {
+          return
+        }
+        if(btn.textContent === '=' && input.value !== '') {
+          input.value = eval(input.value)
+          return
+        }
+        input.value = input.value + btn.textContent;
+      })
+    })
+  }
+
+
+
+  useEffect(handleClick);
+
   return (
     <div className="App">
         <div className="calculator">
-          <input type="text" readOnly></input>
+          <input readOnly></input>
           <div className="btns">
             <Btn number="1"/>
             <Btn number="2"/>
--- calculadora/src/components/btn.jsx
+++ calculadora/src/components/btn.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 
 export default function Btn({number}) {
 
-    function putNumber() {
-        return number;
-    }
-
     return (
-        <button onClick={putNumber}>{number}</button>
+        <button>{number}</button>
     )
 }
