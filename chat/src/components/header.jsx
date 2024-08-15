06e624803db1a4b600fd48a355f8d61a3eaf45dc
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-15 13:18:51 -0400

    install socket live chat API

---
+++ chat/src/components/header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className="bg-gray-800 p-4">
+            <h1 className="text-2xl font-bold text-white">Live Chat</h1>
+        </header>
+    )
+}
