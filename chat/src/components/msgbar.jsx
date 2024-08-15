aa70114a5acec5f20787c6269b82f9dfd0c3eb68
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-15 18:10:06 -0400

    Falta Integrar bien API de SocketIO

--- chat/src/components/msgbar.jsx
+++ chat/src/components/msgbar.jsx
@@ -4,7 +4,7 @@ export default function MsgBar() {
     return (
         <div className="bg-gray-800 p-4 flex absolute bottom-0 w-full">
             <input type="text" className="p-2 w-full outline-none box-border rounded border border-gray-700" placeholder="Type a message..." />
-            <button className="p-2 w-1/4 bg-white rounded border border-gray-700">Send</button>
+            <button className="p-2 w-1/4 transition-all hover:w-2/5 bg-white rounded border border-gray-700">Send</button>
         </div>
     )
 }
