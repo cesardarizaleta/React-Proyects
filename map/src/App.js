9587c7a9eabddaca134edcf301e725ae31f8c6d5
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-16 18:32:03 -0400

    Agregando Recursos

--- map/src/App.js
+++ map/src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="h-screen flex flex-col items-center justify-center">
+      <div id='content' className=''>
+        <h1 className='text-4xl font-bold'>Map</h1>
+        <div></div>
+      </div>
     </div>
   );
 }
