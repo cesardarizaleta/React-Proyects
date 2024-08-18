952909ae55019f2aae777c9725773099ddbe972a
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-18 13:27:48 -0400

    Vinculando co UVCanvas Npm

--- authenticator/src/App.js
+++ authenticator/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
+import Login from './components/login';
+import { Lumiflex } from "uvcanvas"
 
 const App = () => {
   return (
-    <div className="h-screen bg-orange-500 p-8 flex flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold text-white">¡Hola Tailwind CSS!</h1>
-      <p className="mt-4 text-white">
-        Tailwind CSS es increíblemente poderoso. ¡No puedo esperar para seguir explorándolo!
-      </p>
+    <div className="h-screen flex">
+      <Login />
+      <Lumiflex />
     </div>
   );
 }
