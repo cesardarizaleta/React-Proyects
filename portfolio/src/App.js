97625b5fe3a98605858988fb1fce57bbc3205609
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-18 19:26:06 -0400

    Agregando UVCanvas y distribuyendo

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Opulento } from "uvcanvas"
+import './App.css';
+import Main from './components/main';
+
 
 const App = () => {
   return (
-    <div className="h-screen bg-orange-500 p-8 flex flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold text-white">¡Hola Tailwind CSS!</h1>
-      <p className="mt-4 text-white">
-        Tailwind CSS es increíblemente poderoso. ¡No puedo esperar para seguir explorándolo!
-      </p>
+    <div className="h-screen">
+        <Opulento />
+        <Main />
     </div>
   );
 }
---
+++ portfolio/src/components/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Main(){
+    return (
+        <div id='content' className='absolute top-0 left-0 h-screen w-screen'>
+
+            <div id="personal-data">
+
+            </div>
+
+            <div id="knowledge">
+
+            </div>
+          
+        </div>
+    )
+}
