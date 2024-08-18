974967ba3470ce43e375b0750ad153093e29551f
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-18 14:08:08 -0400

    Login frontend hecho

--- authenticator/src/components/login.jsx
+++ authenticator/src/components/login.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import './login.css';
 
 export default function Login() {
     return (
-        <div id="login-content" className="flex flex-col h-full absolute w-1/2 p-10 items-center justify-center gap-5">
-            <h1 className="text-4xl font-bold">Login</h1>
+        <div id="login-content" className="flex flex-col h-full absolute w-1/2 bg-gray-950 p-10 items-center justify-center gap-5">
+            <h1 className="text-4xl font-bold text-white">Login</h1>
             <form className="flex flex-col gap-3 w-full justify-center items-center">
-                <input className="w-3/4 p-2 rounded-md outline-none" type="text" placeholder="Username" />
-                <input className="w-3/4 p-2 rounded-md outline-none" type="password" placeholder="Password" />
-                <button type="submit">Login</button>
+                <input className="w-3/4 p-3 rounded-3xl outline-none" type="text" placeholder="Username" />
+                <input className="w-3/4 p-3 rounded-3xl outline-none" type="password" placeholder="Password" />
+                <button className="text-white border w-3/4 p-3 rounded-3xl" type="submit">Login</button>
             </form>
-            <button>I forgot my password</button>
-            <button>Register</button>
+            <button className="text-white underline">I forgot my password</button>
+            <button className="text-black rounded-3xl bg-white w-3/4 p-3">Register</button>
         </div>
     )
 }
