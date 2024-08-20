f4571fc0b1f8e90818f530c30330a435e74cbfc4
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-19 22:52:52 -0400

    Arreglando Mobile Wrap

--- portfolio/src/components/main.jsx
+++ portfolio/src/components/main.jsx
@@ -8,7 +8,7 @@ export default function Main(){
     return (
         <div id='content' className='absolute gap-6 top-0 left-0 h-screen w-screen flex justify-center items-center flex-wrap'>
 
-            <div id="personal-data" className="w-1/5 h-3/4 p-10 flex flex-col gap-4 rounded-lg">
+            <div id="personal-data" className="w-80 h-3/4 p-10 flex flex-col gap-4 rounded-lg">
                 <ProfilePhoto URL={''} />
                 <h1 className="text-white text-center font-bold text-xl">Name</h1>
                 <h2 className="text-white text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum voluptas nisi illum voluptatem suscipit maiores dolor, impedit nemo aliquam, molestias mollitia sed voluptatum debitis, nostrum fugit earum animi incidunt accusantium!</h2>
