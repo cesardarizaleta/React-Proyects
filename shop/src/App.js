597cd6d4f1afcf9d81eba57037e29d43adfa0187
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-22 23:34:59 -0400

    Agregando boton personalizando

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Tranquiluxe } from "uvcanvas"
 import React from 'react';
+import { MdOutlineShoppingCart } from "react-icons/md";
 
 function App() {
   return (
@@ -9,11 +10,12 @@ function App() {
       <div id='marco' className='absolute top-0 left-0 w-screen h-screen'>
         <main className='relative w-full h-full'>
           <div className='absolute top-0 right-0 w-fit p-4 bg-black text-center text-8xl cursor-pointer transition-all'>
-            <h1 className='text-white'>BYTE</h1>
+            <h1 className='text-white font-bold'>BYTE</h1>
           </div>
-          <div className='absolute bottom-0 left-0 w-2/5 h-1/3 bg-black'>
-
-          </div>
+            <button id='btn-gobuy' className='absolute bottom-0 left-0 flex items-center w-1/4 p-4
+             justify-center gap-3 text-black text-xl transition-all'>
+              <MdOutlineShoppingCart className='w-8 h-8' color='black' />Go buy!
+              </button>
         </main>
       </div>
     </div>
---
+++ shop/src/components/list.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './list.css'
+
+export default function List() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
