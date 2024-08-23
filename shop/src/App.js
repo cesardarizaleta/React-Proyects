5d3585b486e38ede58ea5c12c01346f111b41900
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-22 23:43:22 -0400

    Transicion entre list y main

--- shop/src/App.js
+++ shop/src/App.js
@@ -2,22 +2,33 @@ import './App.css';
 import { Tranquiluxe } from "uvcanvas"
 import React from 'react';
 import { MdOutlineShoppingCart } from "react-icons/md";
+import List from './components/list'
 
 function App() {
+
+  function movetoBuy() {
+    const marco = document.getElementById('marco')
+    const lista = document.querySelector('#list')
+
+    marco.style.top = '-100%'
+    lista.style.top = '0'
+  }
+
   return (
     <div id='app' className="w-screen h-screen">
-      <Tranquiluxe className="" />
-      <div id='marco' className='absolute top-0 left-0 w-screen h-screen'>
+      <Tranquiluxe />
+      <div id='marco' className='absolute top-0 left-0 w-screen h-screen transition-all duration-1000'>
         <main className='relative w-full h-full'>
           <div className='absolute top-0 right-0 w-fit p-4 bg-black text-center text-8xl cursor-pointer transition-all'>
             <h1 className='text-white font-bold'>BYTE</h1>
           </div>
-            <button id='btn-gobuy' className='absolute bottom-0 left-0 flex items-center w-1/4 p-4
+            <button onClick={movetoBuy} id='btn-gobuy' className='absolute bottom-0 left-0 flex items-center w-1/4 p-4
              justify-center gap-3 text-black text-xl transition-all'>
               <MdOutlineShoppingCart className='w-8 h-8' color='black' />Go buy!
               </button>
         </main>
       </div>
+      <List />
     </div>
   );
 }
--- shop/src/components/list.jsx
+++ shop/src/components/list.jsx
@@ -3,8 +3,8 @@ import './list.css'
 
 export default function List() {
     return (
-        <div>
-            
+        <div id="list" className="absolute top-full bg-black left-0 w-3/4 h-screen transition-all duration-1000">
+
         </div>
     )
 }
