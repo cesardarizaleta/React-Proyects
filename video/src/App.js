2ea12caa9dffefbf64d6bf6df6f9d13e312a4eb5
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-23 11:25:22 -0400

    Creando Video y boton

--- video/src/App.js
+++ video/src/App.js
@@ -2,7 +2,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div id="App" className="h-screen w-screen flex flex-col items-center
+    justify-center gap-4">
+      <div className="font-bold text-2xl">
+        <h1>Title</h1>
+      </div>
+      <video className="w-1/2 h-1/2 bg-black rounded-xl"></video>
+      <button className="w-1/2 bg-blue-400 text-white p-2
+      rounded-md transition-all hover:bg-blue-500
+      font-bold hover:rounded-xl active:scale-95">Import Playlist</button>
     </div>
   );
 }
