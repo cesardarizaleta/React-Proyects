0943b9e924e5ece38eb3eb9acf6c70b9352b8dc4
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-23 19:25:56 -0400

    Falta Implementar API

--- weather/src/components/weather.jsx
+++ weather/src/components/weather.jsx
@@ -4,11 +4,11 @@ import './weather.css';
 export default function Weather() {
     return (
         <main className="w-64 h-64 rounded-lg absolute
-        flex flex-col justify-center items-center">
+        flex flex-col justify-center items-center gap-2">
             <h1 className="font-bold text-xl text-white">Weather App</h1>
-            <input type="text" placeholder="Enter city name" />
+            <input className="p-2 rounded-md outline-none" type="text" placeholder="Enter city name" />
             <h3></h3>
-            <h3></h3>
+            <h3 className="font-bold text-white text-7xl">23</h3>
         </main>
     )
 }
