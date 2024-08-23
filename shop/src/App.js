3b61ebed965965e87fd492ca596f7c8ef50e3b23
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-22 23:53:06 -0400

    Colocando Goback y useffect

--- shop/src/App.js
+++ shop/src/App.js
@@ -3,16 +3,26 @@ import { Tranquiluxe } from "uvcanvas"
 import React from 'react';
 import { MdOutlineShoppingCart } from "react-icons/md";
 import List from './components/list'
+import { useEffect } from 'react';
 
 function App() {
 
-  function movetoBuy() {
+  useEffect(() => {
     const marco = document.getElementById('marco')
     const lista = document.querySelector('#list')
+    const btnBack = document.getElementById('btn-back')
+    const btnGoBuy = document.getElementById('btn-gobuy')
 
-    marco.style.top = '-100%'
-    lista.style.top = '0'
-  }
+    btnBack.addEventListener('click', () => {
+      marco.style.top = '0'
+      lista.style.top = '100%'
+    })
+
+    btnGoBuy.addEventListener('click', () => {
+      marco.style.top = '-100%'
+      lista.style.top = '0'
+    })
+  },[])
 
   return (
     <div id='app' className="w-screen h-screen">
@@ -22,7 +32,7 @@ function App() {
           <div className='absolute top-0 right-0 w-fit p-4 bg-black text-center text-8xl cursor-pointer transition-all'>
             <h1 className='text-white font-bold'>BYTE</h1>
           </div>
-            <button onClick={movetoBuy} id='btn-gobuy' className='absolute bottom-0 left-0 flex items-center w-1/4 p-4
+            <button id='btn-gobuy' className='absolute bottom-0 left-0 flex items-center w-1/4 p-4
              justify-center gap-3 text-black text-xl transition-all'>
               <MdOutlineShoppingCart className='w-8 h-8' color='black' />Go buy!
               </button>
--- shop/src/components/list.jsx
+++ shop/src/components/list.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import './list.css'
+import { IoCaretBack } from "react-icons/io5";
 
 export default function List() {
     return (
-        <div id="list" className="absolute top-full bg-black left-0 w-3/4 h-screen transition-all duration-1000">
-
+        <div id="list" className="absolute top-full p-5 bg-black left-0 w-3/4 h-screen transition-all duration-1000">
+            <button id="btn-back" className="bg-white p-3
+            transition-all">
+            <IoCaretBack className="rotate-90 w-6 h-6" />
+            </button>
         </div>
     )
 }
