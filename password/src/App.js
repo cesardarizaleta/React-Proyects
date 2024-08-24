7fcea1f4d1cc00eb280bfed15784332a71613137
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-24 11:00:14 -0400

    Creando Fondo

--- password/src/App.js
+++ password/src/App.js
@@ -2,7 +2,13 @@ import './App.css';
 
 function App() {
   return (
-    <div id="App">
+    <div id="App" className="h-screen w-screen grid place-items-center">
+
+      <main className="w-72 h-96 rounded-xl shadow-xl">
+        <label for="pass">Password</label>
+        <input id="pass"></input>
+      </main>
+
     </div>
   );
 }
