1d8c056fb7fd71d1e1370b92a8c83c5b1a7841a4
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-24 11:34:34 -0400

    Agregando Progressbar

--- password/src/App.js
+++ password/src/App.js
@@ -1,16 +1,32 @@
 import './App.css';
 
 function App() {
+
+  function verify() {
+    alert('hi')
+  }
+
   return (
-    <div id="App" className="h-screen w-screen grid place-items-center">
+    <div id="App" className="h-screen w-screen flex flex-col justify-center items-center
+    gap-2">
 
       <main className="w-72 h-fit hover:w-80 transition-all duration-700
        text-white rounded-xl shadow-xl
       ">
-        <input className="w-full rounded-md p-5
+        <input onChange={verify} className="w-full rounded-md p-5
         cursor-pointer bg-transparent placeholder:text-white
-         outline-none" placeholder="Enter your Password" id="pass"></input>
+        text-center outline-none" placeholder="Enter your Password" id="pass"></input>
       </main>
+
+      <div id="progress" className="w-72 h-8 hover:w-80 transition-all duration-700
+       text-white rounded-md overflow-hidden relative shadow-xl
+      ">
+        <div id="barra"
+        className="absolute left-0 h-full opacity-50 w-6 bg-blue-200">
+
+        </div>
+      </div>
+
 
     </div>
   );
