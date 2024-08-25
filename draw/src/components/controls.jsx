d79fb7b1f7e3ca4fff1b637321d8cfdd2eda6878
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-24 23:52:13 -0400

    listo draw

--- draw/src/components/controls.jsx
+++ draw/src/components/controls.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './controls.css';
 import { RiPencilFill } from "react-icons/ri";
 import { PiPaintBucketFill } from "react-icons/pi";
@@ -8,15 +8,28 @@ import { RiCheckboxBlankFill } from "react-icons/ri";
 import { RiCheckboxBlankCircleFill } from "react-icons/ri";
 
 export default function Controls() {
+
+    useEffect(() => {
+        const controls = document.querySelectorAll('.control-item');
+            controls.forEach(control => {
+                control.addEventListener('click', () => {
+                    controls.forEach(control => control.classList.remove('active'));
+                    control.classList.add('active');
+                }
+                )
+            }
+        )
+    },[])
+
     return (
         <div id="marco-controls" className="w-20 h-96 rounded-lg gap-6 flex flex-col items-center
         *:cursor-pointer *:transition-all *:duration-700 justify-center">
-            <RiPencilFill className="w-8 h-8 control-item rounded-full" />
-            <PiPaintBucketFill className="w-8 h-8 control-item rounded-full" />
-            <FaMarker className="w-7 h-7 control-item rounded-full" />
-            <FaCropAlt className="w-8 h-8 control-item rounded-full" />
-            <RiCheckboxBlankFill className="w-8 h-8 control-item rounded-full" />
-            <RiCheckboxBlankCircleFill className="w-8 h-8 control-item rounded-full" />
+            <RiPencilFill className="w-8 h-8 control-item" />
+            <PiPaintBucketFill className="w-8 h-8 control-item" />
+            <FaMarker className="w-7 h-7 control-item" />
+            <FaCropAlt className="w-8 h-8 control-item" />
+            <RiCheckboxBlankFill className="w-8 h-8 control-item" />
+            <RiCheckboxBlankCircleFill className="w-8 h-8 control-item" />
         </div>
     )
 }
