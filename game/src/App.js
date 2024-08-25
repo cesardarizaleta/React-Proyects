f6fe5a4d3180d8c5a048f3b088dfd4377223d371
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-25 01:04:33 -0400

    Agregando letras

--- game/src/App.js
+++ game/src/App.js
@@ -2,8 +2,17 @@ import './App.css';
 
 function App() {
   return (
-    <div id="App">
-    </div>
+    <main id="App" className="h-screen w-screen bg-center bg-cover flex flex-col justify-center
+    items-center">
+      <div className="w-1/4 h-16 rounded-lg bg-white overflow-hidden p-4 flex gap-2 justify-center items-center">
+        <input maxlength='1' className="outline-none w-10 h-10 text-center"></input>
+        <input maxlength='1' className="outline-none w-10 h-10 text-center"></input>
+        <input maxlength='1' className="outline-none w-10 h-10 text-center"></input>
+        <input maxlength='1' className="outline-none w-10 h-10 text-center"></input>
+        <input maxlength='1' className="outline-none w-10 h-10 text-center"></input>
+        <input maxlength='1' className="outline-none w-10 h-10 text-center"></input>
+      </div>
+    </main>
   );
 }
 
