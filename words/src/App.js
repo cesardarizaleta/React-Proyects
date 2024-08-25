ba07d8797ce76f270c7c9bd1d47996dfbb4d82e2
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-25 00:38:04 -0400

    Creando useState del numero y componente

--- words/src/App.js
+++ words/src/App.js
@@ -1,8 +1,31 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Text from './components/text';
 
 function App() {
+
+  const [words, setWords] = useState(0);
+
+  useEffect(() => {
+    const count = document.getElementById('count');
+    const text = document.querySelector('textarea');
+
+    text.addEventListener('input', () => {
+      const wordstotal = text.value.split(' ').filter(word => word !== '').length;
+      setWords(wordstotal);
+    }
+    )
+  }, []);
+
   return (
-    <div id="App">
+    <div id="App" className="h-screen w-screen bg-cover bg-center flex
+    justify-center items-center gap-4" >
+      <Text />
+      <div id='count' className='w-52 h-2/5 rounded-lg flex flex-col justify-center items-center'>
+        <span><p className='text-white text-center font-bold'>{words}</p></span>
+        <p className='text-white text-center font-bold text-4xl'>Words</p>
+
+      </div>
     </div>
   );
 }
---
+++ words/src/components/text.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './text.css';
+
+export default function Text() {
+
+    
+
+    return (
+        <textarea className="rounded-lg resize-none outline-none text-white h-2/5
+        p-6 text-center font-bold w-64">
+        </textarea>
+    )
+}
