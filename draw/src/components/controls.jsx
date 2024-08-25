37e44b031aa7241c2481c4c19fffd81ff1722d1a
cesardarizaleta <141669141+cesardarizaleta@users.noreply.github.com>
2024-08-24 20:10:07 -0400

    agregando items

--- draw/src/components/controls.jsx
+++ draw/src/components/controls.jsx
@@ -11,12 +11,12 @@ export default function Controls() {
     return (
         <div id="marco-controls" className="w-20 h-96 rounded-lg gap-6 flex flex-col items-center
         *:cursor-pointer *:transition-all *:duration-700 justify-center">
-            <RiPencilFill className="w-8 h-8" />
-            <PiPaintBucketFill className="w-8 h-8" />
-            <FaMarker className="w-7 h-7" />
-            <FaCropAlt className="w-8 h-8" />
-            <RiCheckboxBlankFill className="w-8 h-8" />
-            <RiCheckboxBlankCircleFill className="w-8 h-8" />
+            <RiPencilFill className="w-8 h-8 control-item rounded-full" />
+            <PiPaintBucketFill className="w-8 h-8 control-item rounded-full" />
+            <FaMarker className="w-7 h-7 control-item rounded-full" />
+            <FaCropAlt className="w-8 h-8 control-item rounded-full" />
+            <RiCheckboxBlankFill className="w-8 h-8 control-item rounded-full" />
+            <RiCheckboxBlankCircleFill className="w-8 h-8 control-item rounded-full" />
         </div>
     )
 }
